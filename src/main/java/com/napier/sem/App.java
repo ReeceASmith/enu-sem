1dc3513207d8b24d61823b416cf837c87fcb5531
Reece Smith <40872343+reeceasmith@users.noreply.github.com>
2024-02-11 23:31:37 +0000

    Moved database connect/disconnect into distinct functions

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,8 +4,15 @@ import java.sql.*;
 
 public class App
 {
-    public static void main(String[] args)
-    {
+    /**
+     * Connection to MySQL database
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect() {
         try {
             // Load database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -14,10 +21,8 @@ public class App
             System.exit(-1);
         }
 
-        // Connect to database
-        Connection con = null;
         int retries = 10;
-        for (int i = 0; i < retries; ++i) {
+        for (int i=0; i<retries; ++i) {
             System.out.println("Connecting to database...");
             try {
                 // Wait for db to start
@@ -25,18 +30,19 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "password");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(5000);
-                // Exit
                 break;
-            } catch (SQLException e) {
-                System.out.println("Failed to connect to database attempt " + i);
-                System.out.println(e.getMessage());
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database, attempt " + i);
+                System.out.println(sqle.getMessage());
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+
+
+    public void disconnect() {
         if (con != null) {
             try {
                 // Close connection
@@ -45,5 +51,19 @@ public class App
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
